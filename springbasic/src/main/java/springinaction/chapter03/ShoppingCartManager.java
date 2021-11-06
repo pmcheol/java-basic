e1cf21f398c1ecdc6c6d3c1968e5b77917faa117
민철 <mincheol@mincheol-MacBookPro.local>
2021-11-06 18:18:45 +0900

    ShoppingCartManager 추가

---
+++ springbasic/src/main/java/springinaction/chapter03/ShoppingCartManager.java
@@ -0,0 +1,10 @@
+package springinaction.chapter03;
+
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.INTERFACES)
+//@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ShoppingCartManager {
+}
