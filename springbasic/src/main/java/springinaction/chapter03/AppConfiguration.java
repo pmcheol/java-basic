65b4267dc5045e5150fcd625ac86744b5ac893cd
민철 <mincheol@mincheol-MacBookPro.local>
2021-11-07 17:04:55 +0900

    SpEL 추가

--- springbasic/src/main/java/springinaction/chapter03/AppConfiguration.java
+++ springbasic/src/main/java/springinaction/chapter03/AppConfiguration.java
@@ -1,21 +1,20 @@
 package springinaction.chapter03;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+
+import java.util.Properties;
 
 @PropertySource({"classpath:prop.properties"})
 @ComponentScan
 @Configuration
 public class AppConfiguration {
-    @Autowired
-    private Environment environment;
-
     @Bean
-    public RockBand rockBand() {
-        return new RockBand(environment.getProperty("band.buzz.name"));
+    public RockBand rockBand(@Value("#{ systemProperties }") Properties properties) {
+        properties.list(System.out);
+        return new RockBand(null);
     }
 }
