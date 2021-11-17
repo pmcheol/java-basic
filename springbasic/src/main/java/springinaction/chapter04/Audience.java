58bd7ed5b84457c64857c931cea0e2be14c62c55
민철 <mincheol@mincheol-MacBookPro.local>
2021-11-18 00:27:53 +0900

    @Pointcut

--- springbasic/src/main/java/springinaction/chapter04/Audience.java
+++ springbasic/src/main/java/springinaction/chapter04/Audience.java
@@ -1,23 +1,24 @@
 package springinaction.chapter04;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Audience {
-    @Before("execution(** springinaction.chapter04.Performance.perform(..))")
+    @Pointcut("execution(* springinaction.chapter04.Performance.perform(..))")
+    public void performancePerform() {
+    }
+
+    @Before("performancePerform()")
     public void takeSeats() {
         System.out.println("taking seats.");
     }
 
-    @AfterReturning("execution(** springinaction.chapter04.Performance.perform(..))")
+    @AfterReturning("performancePerform()")
     public void applause() {
         System.out.println("CLAP! CLAP! CLAP!");
     }
 
-    @AfterThrowing("execution(** springinaction.chapter04.Performance.perform(..))")
+    @AfterThrowing("performancePerform()")
     public void demandRefund() {
         System.out.println("Demanding a refund!");
     }
